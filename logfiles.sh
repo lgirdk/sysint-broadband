6da1851c8dc83978b65ab9cb0b647dd80aac1b8c
Andre McCurdy <armccurdy@gmail.com>
2021-03-12 14:56:13 -0800

    revert back to setting SYS_DB_FILE to /nvram/syscfg.db in all cases
    The recent upstream change to control SYSCFG_DB_FILE based on
    BOX_TYPE is not easy to maintain. Hardcode the value to match the
    legacy behaviour when the UpdateNvram syscfg value was set to true.
    
    It's not necessary (or safe - since the value may not exist) to test
    the UpdateNvram syscfg value.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- logfiles.sh
+++ logfiles.sh
@@ -66,12 +66,7 @@ FLUSH_LOG_PATH="/rdklogger/flush_logs.sh"
 SYS_CFG_FILE="syscfg.db"
 BBHM_CFG_FILE="bbhm_cur_cfg.xml"
 WIRELESS_CFG_FILE="wireless"
-
-if [ "$BOX_TYPE" = "XB3" ]; then
 SYS_DB_FILE="/nvram/syscfg.db"
-else
-SYS_DB_FILE="/opt/secure/data/syscfg.db"
-fi
 
 moveFile()
 {        
