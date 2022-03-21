e2a3ceb1f4b7aec9f8c86af9d0fb869aa73fee83
Tarun Saha <tsaha@libertyglobal.com>
2022-03-21 12:51:39 -0700

    dmcli getv optimization
    Adapt scripts to use 'retv' option to get non-verbose parameter value.

--- opsLogUpload.sh
+++ opsLogUpload.sh
@@ -81,7 +81,7 @@ else
    mTlsLogUpload=`syscfg get mTlsLogUpload_Enable`
 fi
 
-encryptionEnable=`dmcli eRT getv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.EncryptCloudUpload.Enable | grep value | cut -d ":" -f 3 | tr -d ' '`
+encryptionEnable=`dmcli eRT retv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.EncryptCloudUpload.Enable`
 URLENCODE_STRING=""
 
 checkXpkiMtlsBasedLogUpload()
@@ -117,7 +117,7 @@ timeToUpload=`date`
 LOG_FILE=$MAC"_Logs_$dt.tgz"
 PATTERN_FILE="/tmp/pattern_file"
 WAN_INTERFACE=$(getWanInterfaceName)
-SECONDV=`dmcli eRT getv Device.X_CISCO_COM_CableModem.TimeOffset | grep value | cut -d ":" -f 3 | tr -d ' ' `
+SECONDV=`dmcli eRT retv Device.X_CISCO_COM_CableModem.TimeOffset`
 UPLOAD_LOG_STATUS="/tmp/upload_log_status"
 SYS_DB_FILE="/nvram/syscfg.db"
 
