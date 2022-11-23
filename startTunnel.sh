9df13a84adbeea905a6604649821bd98c442931b
Devin Mungan <dmungan@libertyglobal.com>
2022-11-23 15:21:59 +0100

    ReverseSSH: notify rssh service start and stop via telemetry ( OFW-3754 )
    Implement telemetry logging in rssh service.
    
    [Issue]
    RSSH feature shall use telemetry and component logging shall also be improved.
    
    [Solution]
    Add telemetry capacity and start&stop events to startTunnel.sh
    
    [Testing Performed]
    1) Set RSSH arguments via below command
    	dmcli eRT setv Device.DeviceInfo.X_LGI-COM_DeviceMgmt.ReverseSSH.Arguments string \
    		'host=<IP>;user=<user>;passwd=<password>;revsshport=<port>'
    2) start RSSH feature
    	dmcli eRT setv Device.DeviceInfo.X_LGI-COM_DeviceMgmt.ReverseSSH.Enable bool true
    3) Query tunnel status and confirm it's ACTIVE
    	dmcli eRT getv Device.DeviceInfo.X_LGI-COM_DeviceMgmt.ReverseSSH.TunnelStatus
    4) Observe that telemetry events are received with marker EVT_SYS_SH_RSSH_split
    
    Upstream Candidate: No, LG specific.
    
    Signed-off-by: Devin Mungan(dmungan@libertyglobal.com)

--- startTunnel.sh
+++ startTunnel.sh
@@ -23,6 +23,7 @@
 . /etc/device.properties
 
 source /etc/waninfo.sh
+source /lib/rdk/t2Shared_api.sh
 
 if [ -f /lib/rdk/t2Shared_api.sh ]; then
                 source /lib/rdk/t2Shared_api.sh
@@ -32,6 +33,7 @@ SSH_CLIENT="/usr/bin/dbclient"
 SSH_DAEMON="/usr/sbin/dropbear"
 WAN_INTERFACE=$(getWanInterfaceName)
 STARTUNNELSH=$0
+MARKER="EVT_SYS_SH_RSSH_split"
 
 usage()
 {
@@ -46,6 +48,7 @@ startSshClientandDaemon()
 
   flock "$fd"
   (
+    t2ValNotify "$MARKER" "RSSH Service Started"
     # Start SSH daemon on demand
     start-stop-daemon -S -x $SSH_DAEMON -m -b -p /var/tmp/rsshd.pid -- -B -F
     DROPBEAR_PASSWORD="$PASSWD" start-stop-daemon -S -x $SSH_CLIENT -m -p /var/tmp/rssh.pid -- $args
@@ -200,10 +203,15 @@ case $oper in
                  if [ -f /var/tmp/rssh.pid ];then
                      start-stop-daemon -K -p /var/tmp/rssh.pid
                      rm /var/tmp/rssh.pid
+                     sendEvent=1
                  fi
                  if [ -f /var/tmp/rsshd.pid ];then
                      start-stop-daemon -K -p /var/tmp/rsshd.pid
                      rm /var/tmp/rsshd.pid
+                     sendEvent=1
+                 fi
+                 if [ $sendEvent -eq 1 ];then
+                     t2ValNotify "$MARKER" "RSSH Service Stopped"
                  fi
                  exit 1
              fi
