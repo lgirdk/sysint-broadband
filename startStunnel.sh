ae8b362413279f1acea83e2e2c9e38a34cd7033f
Vinothkumar <vinothkumar_baskaran@comcast.com>
2023-11-14 15:41:30 +0000

    RDKB-53858 Migrate stunnel to use P12 cert
    Reason for change: To integrate SE based device cert for mTls connectivity in stunnel
    Test Procedure: Build and verify SHORTS connectivity
    Risks: None
    Priority: P1
    
    Change-Id: Idf268d6622a64af8c4de83b2b794fc539b8942d1
    Signed-off-by: Vinothkumar <vinothkumar_baskaran@comcast.com>
    (cherry picked from commit 9c5bc2329edb0c614b6b96a75d5104990c2d88a4)

--- startStunnel.sh
+++ startStunnel.sh
@@ -22,22 +22,13 @@ if [ $# -lt 5 ]; then
    exit 1
 fi
 
-# don't do anything if the feature is disabled.
-if [ -f "/lib/rdk/shortsDownload.sh" ]; then
-    isShortsDLEnabled=`syscfg get ShortsDL`
-    if [ "x$isShortsDLEnabled" != "xtrue" ]; then
-        echo_t "ShortsDL RFC disabled, SHORTS feature not available" >> $LOG_FILE
-        exit 0
-    fi
-fi
-
 if [ $DEVICE_TYPE == "broadband" ]; then
     DEVICE_CERT_PATH=/nvram/certs
 elif [ $DEVICE_TYPE == "mediaclient" -o $DEVICE_TYPE == "hybrid" ]; then
     DEVICE_CERT_PATH=/opt/certs
 else
-    echo_t "STUNNEL: $DEVICE_CERT_PATH, not expected"
-    DEVICE_CERT_PATH=/tmp/certs
+    echo_t "STUNNEL: Unexpected device type: $DEVICE_TYPE"
+    exit 1
 fi
 
 #collect the arguments
@@ -52,8 +43,7 @@ REVERSESSHARGS=$5
 SHORTSHOSTLOGIN=$6
 NONSHORTSHOSTLOGIN=$7
 
-SOURCE_IP=$(echo "$NONSHORTSHOSTLOGIN" | cut -d'@' -f2)
-t2ValNotify "SSH_INFO_SOURCE_IP" "$SOURCE_IP"
+t2ValNotify "SSH_INFO_SOURCE_IP" "$JUMP_SERVER"
 isShortsenabled=$(dmcli eRT getv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.SHORTS.Enable | grep value | cut -d ":" -f 3 | tr -d ' ')
 echo_t "isShortsenabled = $isShortsenabled " >> $LOG_FILE
 if [ $isShortsenabled == "false" ];then
@@ -61,30 +51,23 @@ if [ $isShortsenabled == "false" ];then
         exit 0
 fi
 
-
-#RDM parameters
-DNLD_SCRIPT=/lib/rdk/shortsDownload.sh
-SOCAT_PATH=/tmp/socat_dnld/usr/bin/socat
-STUNNEL_PATH=/tmp/stunnel_dnld/usr/bin/stunnel
-
-
 STUNNEL_PID_FILE=/tmp/stunnel_$LOCAL_PORT.pid
 REVSSH_PID_FILE=/var/tmp/rssh.pid
 STUNNEL_CONF_FILE=/tmp/stunnel_$LOCAL_PORT.conf
 
 echo  "pid = $STUNNEL_PID_FILE"           > $STUNNEL_CONF_FILE
-echo "output=$LOG_FILE"   		 >> $STUNNEL_CONF_FILE
-echo "debug = 7"                       	 >> $STUNNEL_CONF_FILE
+echo  "output=$LOG_FILE"                 >> $STUNNEL_CONF_FILE
+echo  "debug = 7"                        >> $STUNNEL_CONF_FILE
 echo  "[ssh]"                            >> $STUNNEL_CONF_FILE
 echo  "client = yes"                     >> $STUNNEL_CONF_FILE
 
 # Use localhost to listen on both IPv4 and IPv6
-echo "accept = localhost:$LOCAL_PORT"     >> $STUNNEL_CONF_FILE
+echo "accept = localhost:$LOCAL_PORT"    >> $STUNNEL_CONF_FILE
 echo "connect = $JUMP_SERVER:$JUMP_PORT" >> $STUNNEL_CONF_FILE
 
 extract_stunnel_client_cert
 
-if [ ! -f $CERT_FILE -o ! -f $CA_FILE ]; then
+if [ ! -f $CERT_PATH -o ! -f $CA_FILE ]; then
     echo_t "STUNNEL: Required cert/CA file not found. Exiting..." >> $LOG_FILE
     t2CountNotify "SHORTS_STUNNEL_CERT_FAILURE"
     exit 1
@@ -95,10 +78,10 @@ DEV_SAN=tstcpedev.xcal.tv
 PROD_SAN=tstcpeprod.xcal.tv
 
 # this might change once we get proper certificates
-echo "cert = $CERT_FILE"                     			      >> $STUNNEL_CONF_FILE
-echo "CAfile = $CA_FILE"                                              >> $STUNNEL_CONF_FILE
-echo "verifyChain = yes"                                              >> $STUNNEL_CONF_FILE
-echo "checkHost = $JUMP_FQDN"                                         >> $STUNNEL_CONF_FILE
+echo "cert = $CERT_PATH"                 >> $STUNNEL_CONF_FILE
+echo "CAfile = $CA_FILE"                 >> $STUNNEL_CONF_FILE
+echo "verifyChain = yes"                 >> $STUNNEL_CONF_FILE
+echo "checkHost = $JUMP_FQDN"            >> $STUNNEL_CONF_FILE
 
 DEVICETYPE=$(dmcli eRT getv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Identity.DeviceType | grep value | cut -d ":" -f 3 | tr -d ' ')
 echo_t "STUNNEL: Device type is $DEVICETYPE"
@@ -106,7 +89,7 @@ if [ ! -z "$DEVICETYPE" ]; then
     if [ "$DEVICETYPE" == "TEST" ] || [ "$DEVICETYPE" == "test" ];  then
         echo_t "STUNNEL: Device type is TEST" >> $LOG_FILE
         t2CountNotify "SHORTS_DEVICE_TYPE_TEST"
-        echo "checkHost   = $DEV_SAN"          >> $STUNNEL_CONF_FILE
+        echo "checkHost   = $DEV_SAN"    >> $STUNNEL_CONF_FILE
     else
         echo_t "STUNNEL: Device type is PROD" >> $LOG_FILE
         t2CountNotify "SHORTS_DEVICE_TYPE_PROD"
@@ -117,26 +100,9 @@ else
     t2CountNotify "SHORTS_DEVICE_TYPE_UNKNOWN"
 fi
 
+export P12PASSCODE=$(eval "$PASSCODE")
 
-if [ -f "/usr/bin/stunnel" ]; then
-    /usr/bin/stunnel $STUNNEL_CONF_FILE
-elif [ -f $STUNNEL_PATH ]; then
-    $STUNNEL_PATH $STUNNEL_CONF_FILE
-else
-    # Ideally shorts(socat and stunnel) packages should download at bootup time.
-    # In case if some problem in downloading, shorts pacakages shell download here
-    echo_t "stunnel/socat not found, need to download" >> $LOG_FILE
-    sh $DNLD_SCRIPT
-    DNLD_RES=$?
-    if [ $DNLD_RES -eq 0 ]; then
-        $STUNNEL_PATH $STUNNEL_CONF_FILE
-    else
-        echo_t "RDM not able to download shorts packages" >> $LOG_FILE
-        rm -f $STUNNEL_CONF_FILE
-        rm -f $D_FILE
-        exit 1
-    fi
-fi
+/usr/bin/stunnel $STUNNEL_CONF_FILE
 
 # cleanup sensitive files early
 rm -f $STUNNEL_CONF_FILE
@@ -148,7 +114,9 @@ STUNNELPID=`cat $STUNNEL_PID_FILE`
 if [ -z "$STUNNELPID" ]; then
     rm -f $STUNNEL_PID_FILE
     rm -f $CA_FILE
-    rm -f $CERT_FILE
+    if [ "x$CRED_INDEX" == "x0" ]; then
+        touch /tmp/.$SE_DEVICE_CERT
+    fi
     echo_t "STUNNEL: stunnel-client failed to establish. Exiting..." >> $LOG_FILE
     t2CountNotify "SHORTS_STUNNEL_CLIENT_FAILURE"
     exit
@@ -163,6 +131,9 @@ REVSSHPID2=`cat $REVSSH_PID_FILE`
 if [ -z "$REVSSHPID2" ] || [ "$REVSSHPID1" == "$REVSSHPID2" ]; then
     kill -9 $STUNNELPID
     rm -f $STUNNEL_PID_FILE
+    if [ "x$CRED_INDEX" == "x0" ]; then
+        touch /tmp/.$SE_DEVICE_CERT
+    fi
     echo_t "STUNNEL: Reverse SSH failed to connect. Exiting..." >> $LOG_FILE
     t2CountNotify "SHORTS_SSH_CLIENT_FAILURE"
     exit
