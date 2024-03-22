eddb61ad8f1c04544497c2a61532a10814b270af
sgunag263 <SuneethaLakshmi_Gunagunuru@comcast.com>
2024-03-22 10:40:10 +0000

    XER5-445:In XER5 rdklogs are not parity with TXB7
    Reason for change: In XER5 messages.txt log file is not present in
    /rdklogs/logs path
    Test Procedure:messages.txt file should be available in /rdklogs/logs
    path in XER5
    Risks:Low
    Priority:P1
    
    Change-Id: I85f6fe5c1ac7cbde13416e30c5f8a79b8c4f6de6
    Signed-off-by: sgunag263 <SuneethaLakshmi_Gunagunuru@comcast.com>
    (cherry picked from commit 2bd1c880f768ecf826f8ac69d3711ff2cc7b2a65)
    (cherry picked from commit 04092453c581e207c356cce25f3e508cb489d13f)

--- logfiles.sh
+++ logfiles.sh
@@ -277,7 +277,7 @@ syncLogs_nvram2()
 
         fi
 
-	if [ "$BOX_TYPE" == "XB6" ] || [ "$BOX_TYPE" == "XF3" ] || [ "$BOX_TYPE" == "TCCBR" ];then
+	if [ "$BOX_TYPE" == "XB6" ] || [ "$BOX_TYPE" == "XF3" ] || [ "$BOX_TYPE" == "TCCBR" ] ||  [ "$BOX_TYPE" == "VNTXER5" ];then
 		current_time=$(date +%s)
 		   if [ -f "$lastdmesgsync" ];then
 		   	lastsync_time=`cat $lastdmesgsync`
