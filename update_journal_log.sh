f18e75db50b1c497c9fcac98a4a3c98d8d97cbe9
Robert Lee <Robert_Lee2@comcast.com>
2024-06-10 17:42:01 +0000

    XER10-241: adding journal_logs.txt.0
    Reason for change: adding journal_logs.txt.0
    Test Procedure: build and verify
    Risks: Med
    Priority: P1
    
    Change-Id: Ib512b5d2cebdc381c61fcc483d902b497f4b65e2
    Signed-off-by: Robert_Lee2@comcast.com
    (cherry picked from commit 3bb5103c13c39feb8f6eaefb4b5f29262d4b2162)

--- update_journal_log.sh
+++ update_journal_log.sh
@@ -39,7 +39,7 @@ do
    
    #Keeps appending to the existing file 
    nice -n 19 journalctl -k --since "${difference_time} sec ago" >> ${DMESG_FILE}
-   if [ "$BOX_TYPE" = "XB6" ] || [ "$BOX_TYPE" = "XF3" ] || [ "$BOX_TYPE" = "TCCBR" ] || [ "$BOX_TYPE" == "VNTXER5" ];then
+   if [ "$BOX_TYPE" = "XB6" ] || [ "$BOX_TYPE" = "XF3" ] || [ "$BOX_TYPE" = "TCCBR" ] || [ "$BOX_TYPE" == "VNTXER5" ] || [ "$BOX_TYPE" == "SCER11BEL" ];then
 	   #ARRISXB6-7973: Complete journalctl logs to /rdklogs/logs/journal_logs.txt.0
            uptime_in_secs=$(cut -d. -f1 /proc/uptime)
            if [ $uptime_in_secs -ge 240 ]  && [ $BootupLog_is_updated -eq 0 ]; then
